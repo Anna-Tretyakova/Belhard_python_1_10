704376721ab500f179bad5aac946b217c0262dcf
“Anna <anna.tretyakova.minsk@gmail.com>
2022-09-20 00:36:08 +0300

    tasks solved

--- tasks/csv_tasks/population.py
+++ tasks/csv_tasks/population.py
@@ -3,3 +3,15 @@
 файла world_population.csv и функцию должна возвращать год, в котором
 наибольший процент прироста населения.
 """
+import csv
+
+
+def max_delta(file: str):
+    with open(file, 'rt') as inf:
+        data = [(float(row['ChangePerc']), row['Year']) for row in csv.DictReader(inf)]
+        data.sort(reverse=True)
+        list_1 = list(data[0])
+        return(list_1[1])
+
+
+print(max_delta("world_population.csv"))
--- tasks/file_tasks/reverse_rows.py
+++ tasks/file_tasks/reverse_rows.py
@@ -3,3 +3,15 @@
 файл. Создать новый файл, каждая строка которого получается из соответствующей
 строки исходного файла перестановкой слов в обратном порядке.
 """
+
+
+def revert_rows(file: str):
+    with open(file) as file:
+        with open("new file.txt", "w") as w_file:
+            for line in file:
+                temp_list = line.split(' ')
+                reverse_string = ' '.join(temp_list[::-1]) + "\n"
+                w_file.write(reverse_string)
+
+
+revert_rows("text.txt")
